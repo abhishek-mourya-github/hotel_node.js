e204726c4a61be824fc52c12ea620468256a1082
abhishek_mourya <kartikmourya1947@gmail.com>
2025-03-24 21:23:23 +0530

    shift mongodb server online

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // data will be store in req.body
+const PORT = process.env.PORT || 3000;
+
 
 app.get("/", function (req, res) {
   res.send("Welcome to the hotel");
@@ -15,6 +18,6 @@ app.use('/', personRoutes);
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/', menuRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening on port 3000");
 });
