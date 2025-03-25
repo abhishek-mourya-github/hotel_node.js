5d9c0c7d88df13b2067d6fce8fca14b3871b08f9
abhishek_mourya <kartikmourya1947@gmail.com>
2025-03-25 21:59:56 +0530

    here are some micro changes to make it work perfeclty

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const mongoURL = process.env.MONGODB_URL_LOCAL;
 const mongoURL = process.env.MONGODB_URL;
 
-mongoose.connect(mongoURL)
+mongoose.connect(mongoURL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+})
 .then(() => console.log("MongoDB server is connected"))
-.catch((err) => console.error("Error in MongoDB server", err))
+.catch((err) => console.error("Error in MongoDB server", err));
 
 const db = mongoose.connection;
 
 db.on('disconnected', () => {
-    console.log("MongoDB server is disconnected");
-} )
+  console.log("MongoDB server is disconnected");
+});
 
 module.exports = db;
